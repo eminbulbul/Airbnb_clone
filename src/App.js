8c26d51f4e4c946c22facda6b1223428b0f66013
eminbulbul <mehmeteminbul@gmail.com>
2022-05-10 13:34:47 +0300

    home created with css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Home from "./Pages/Home";
 
 function App() {
-  return <div className="App">Airbnb Start</div>;
+  return (
+    <div className="App">
+      Airbnb Start
+      <Home />
+    </div>
+  );
 }
 
 export default App;
