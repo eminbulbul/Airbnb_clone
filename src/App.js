f4bd0bdf208d12722cfb71ee1008c92335092049
m_emin <mehmeteminbul@gmail.com>
2022-05-13 21:05:45 +0300

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
       Airbnb Start
+      <Header />
       <Home />
     </div>
   );
