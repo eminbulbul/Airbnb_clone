eb39290a3e573bc22e617297591df984ffe5cf5c
m_emin <mehmeteminbul@gmail.com>
2022-08-10 22:23:37 +0300

    header done with css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
 const Header = () => {
   return (
     <div className="header">
@@ -8,6 +12,16 @@ const Header = () => {
         src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
         alt=""
       />
+      <div className="header__center">
+        <input type="text" name="" id="" />
+        <SearchIcon />
+      </div>
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
     </div>
   );
 };
