45e67e925111ea5d69f91545424c89acb057a529
eminbulbul <mehmeteminbul@gmail.com>
2022-08-18 13:24:00 +0300

    card style ended footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./Pages/Home";
+// import { BrowserRouter as Router, Switch, Route} from "react-router-dom";s
 
+// make another page for router 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        © 2022 Airbnb Clone! No rights reserved - This is a clone by M.Emin
+        Bulbul
+      </p>
+      <p>Privacy - Terms - Sitemap - Company Details </p>
+    </div>
+  );
+};
+
+export default Footer;
